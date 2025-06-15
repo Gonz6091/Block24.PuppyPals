9acffcdfeaf58cf375768c45eef6b517be71de47
Gonz6091 <michaelgonzalezkelly@gmail.com>
2025-06-15 14:04:50 -0400

    Here is the code for the Puppy Pals workshop with some minimal CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
+import './App.css'
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  console.log(puppies)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
   return (
-      <div></div>
+      <div>
+        {
+        puppies.map((puppy) => {
+          console.log("puppy id: ", puppy.id)
+          return <p className="puppy-list" onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        }) 
+        }
+        {featPupId && (
+        <div className="featured-pup">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      </div>
   )
 }
 
